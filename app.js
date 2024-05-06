e0e4380aa61881cfebb952dd431e1a5a2c71aefd
Tim Yu <chonghao.yu@gmail.com>
2024-05-06 16:39:21 +0800

    chore: 1.install basic mudule express nodemon dotenv mongoose axios 2.設置 env, env file, rules 3.新增 config, model, router folder 4.基本 app.js 配置

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const express  = require('express');
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const dotenv = require('dotenv');
+
+//ENV
+dotenv.config({ path: `${__dirname}/env/.env.local` });
+const PORT = process.env.PORT;
+const MongoDBPath = process.env.MongoDBPath;
+
+//Link DB
+//Mongo
+mongoose.connect(MongoDBPath)
+  .then(() => {
+    console.log("MongoDB Linking ....");
+  })
+  .catch(() => {
+    console.log("MongoDB Linked Error ....");
+  });
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-
-app.listen(1000, ()=>{
+app.listen(PORT, () => {
   console.log("mern api is starting...");
 })
 
