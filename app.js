54c5d79308b8d7041d5570157f01aa02ee7d8893
Tim Yu <chonghao.yu@gmail.com>
2024-06-05 15:12:58 +0800

    feat: 1. 新增允許 all routes CORS。

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
-const dotenv = require('dotenv');
-const User = require('./models/user');
-const Course = require('./models/course');
+const mongoose = require("mongoose");
+const path = require("path");
+const dotenv = require("dotenv");
+const User = require("./models/user");
+const Course = require("./models/course");
 const passport = require("passport");
+const cors = require("cors");
 //觸發 strategy
 require("./config/passport-strategy");
 const userRoutes = require("./routes/user-routes");
@@ -18,7 +19,8 @@ const MongoDBPath = process.env.MONGO_DB_Path;
 
 //Link DB
 //Mongo
-mongoose.connect(MongoDBPath)
+mongoose
+  .connect(MongoDBPath)
   .then(() => {
     console.log("MongoDB Linking ....");
   })
@@ -29,20 +31,21 @@ mongoose.connect(MongoDBPath)
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 //如果此 Route 都需要驗證 token，故把 middleware 寫在此
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 const authenticated = (req, res, next) => {
-  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+  passport.authenticate("jwt", { session: false }, (err, user, info) => {
     // console.log(`err -> ${err}`);
     // console.log(`user-> ${user}`);
     // console.log(`info-> ${info}`);
     if (err) {
       return res.json({
         code: 411,
-        result: 'Token 錯誤或失效',
-        errorMessage: `${err}`
-      })
+        result: "Token 錯誤或失效",
+        errorMessage: `${err}`,
+      });
     }
 
     if (user) {
@@ -52,15 +55,13 @@ const authenticated = (req, res, next) => {
 
     return res.json({
       code: 412,
-      result: 'Token 錯誤或失效',
-      errorMessage: `${info}`
+      result: "Token 錯誤或失效",
+      errorMessage: `${info}`,
     });
-  })(req, res, next)
-}
-app.use('/api/course', authenticated, courseRoutes);
-
+  })(req, res, next);
+};
+app.use("/api/course", authenticated, courseRoutes);
 
 app.listen(PORT, () => {
   console.log(`mern api port ${PORT} is starting...`);
-})
-
+});
